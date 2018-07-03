a99a4f19866aaa39b14e2239e6baa81ec62a9b7c
sindunuragarp <sindunuragarp@gmail.com>
2018-07-03 14:21:19 +0200

    Implemented minimal requirements

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
-from flask import Flask
+from flask import Flask, Response
+from jokes import get_jokes, fill_jokes, flush_jokes
+import json
 
 app = Flask(__name__)
 
 
+@app.before_first_request
+def startup():
+    fill_jokes()
+
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'It Works!'
+
+
+@app.route('/getJokes')
+def jokes_get():
+    data = get_jokes()
+
+    res = {
+        'status': 'success',
+        'jokes': data
+    }
+    return Response(json.dumps(res), status=200, mimetype='application/json')
+
+
+@app.route('/getNewJokes')
+def jokes_get_new():
+    fill_jokes()
+    data = get_jokes()
+
+    res = {
+        'status': 'success',
+        'jokes': data
+    }
+    return Response(json.dumps(res), status=200, mimetype='application/json')
+
+
+@app.route('/flushJokes')
+def jokes_flush():
+    flush_jokes()
+
+    res = {
+        'status': 'success'
+    }
+    return Response(json.dumps(res), status=200, mimetype='application/json')
 
 
 if __name__ == '__main__':
---
+++ jokes.py
@@ -0,0 +1,49 @@
+import requests
+
+url = "http://api.icndb.com/jokes/random/"
+max_jokes = 10
+max_requests = 20
+jokes = []
+
+
+def get_jokes():
+    return jokes
+
+
+def fill_jokes():
+    global jokes
+    diff = max_jokes - len(jokes)
+    new_jokes = _fetch_jokes(diff)
+    jokes.extend(new_jokes)
+
+
+def flush_jokes():
+    global jokes
+    jokes = []
+
+
+def _fetch_jokes(num):
+    new_jokes = []
+    tries = 0
+
+    while len(new_jokes) < num:
+        tries += 1
+        if tries > max_requests:
+            break
+
+        res = requests.get(url)
+        if res.status_code != 200:
+            continue
+
+        data = res.json()
+        if data['type'] != 'success':
+            continue
+
+        joke = data['value']
+        joke_id = joke['id']
+        if joke_id in [x['id'] for x in jokes]:
+            continue
+
+        new_jokes.append(joke)
+
+    return new_jokes
